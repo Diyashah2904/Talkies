67814537292409be8ad1907ff8e6f4efa7e54923
Ravij-p <ravijbpatel9124@gmail.com>
2025-06-19 23:36:30 +0530

    remove unnecessary tag

--- talkies/src/modules/Authorization/index.js
+++ talkies/src/modules/Authorization/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import Form from "./form";
 const Auth = () => {
   const isSignInPage = window.location.pathname.includes("signin");
-  return (
-    <div>
-      <Form isSignInPage={isSignInPage} />
-    </div>
-  );
+  return <Form isSignInPage={isSignInPage} />;
 };
 
 export default Auth;
