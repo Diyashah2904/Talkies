e62860e900c92afbdb15804380d08e1da3383ae6
Ravij-p <ravijbpatel9124@gmail.com>
2025-06-22 13:54:34 +0530

    upgrade false

--- talkies/src/socket.js
+++ talkies/src/socket.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io("https://talkies-backend.onrender.com", {
   transports: ["polling"],
+  upgrade: false,
   withCredentials: true,
 });
 export default socket;
